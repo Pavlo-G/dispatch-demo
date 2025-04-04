574d6c982bec73caf31fdfd144dffcb13a4ceaf4
Mike A <mike.amatruda@gmail.com>
2025-04-04 12:37:22 -0400

    prune unused imports

--- job-service/src/main/java/com/example/job_service/JobServiceApplication.java
+++ job-service/src/main/java/com/example/job_service/JobServiceApplication.java
@@ -1,12 +1,7 @@
 package com.example.job_service;
 
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class JobServiceApplication {
