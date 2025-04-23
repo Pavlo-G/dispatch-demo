6973f09a13a19074be57c47f1ed4363792e81dad
Mike Dawe <mdawe26@gmail.com>
2025-04-23 16:44:49 -0400

    Added profile page

--- ui-service/src/routes/profile/Profile.tsx
+++ ui-service/src/routes/profile/Profile.tsx
@@ -1,18 +1,79 @@
-import { Box, Typography, Grid } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Button,
+} from "@mui/material";
+import { current } from "@reduxjs/toolkit";
 import CustomizedDataGrid from "src/dashboard/components/CustomizedDataGrid";
 
-export default function Techs() {
+const mockData = {
+  firstName: "John",
+  lastName: "Doe",
+  id: "TECH1243",
+  skills: ["Wireless", "Fiber"],
+  phoneNumber: "123-456-7890",
+};
+
+export default function Profile() {
+  const currentUser = mockData;
   return (
-    <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
-      {/* cards */}
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Profile
-      </Typography>
-      <Grid container spacing={2} columns={12}>
-        <Grid size={{ xs: 12, lg: 9 }}>
-          <CustomizedDataGrid />
-        </Grid>
-      </Grid>
+    <Box
+      component="form"
+      sx={{
+        minWidth: "40dvw",
+        gap: 2,
+        display: "flex",
+        flexDirection: "column",
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="outlined-basic"
+        label="ID"
+        variant="outlined"
+        value={currentUser.id}
+        disabled
+      />
+      <TextField
+        id="outlined-basic"
+        label="First Name"
+        variant="outlined"
+        value={currentUser.firstName}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Last Name"
+        variant="outlined"
+        value={currentUser.lastName}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Phone Number"
+        variant="outlined"
+        value={currentUser.phoneNumber}
+      />
+      <FormGroup>
+        <FormLabel component="legend">Skills</FormLabel>
+        {currentUser.skills.map((skill, index) => (
+          <FormControlLabel
+            key={index}
+            control={
+              <Checkbox checked={!!currentUser.skills.includes(skill)} />
+            }
+            label={skill}
+          />
+        ))}
+      </FormGroup>
+      <Button variant="contained">
+        Save Changes
+      </Button>
     </Box>
   );
 }
