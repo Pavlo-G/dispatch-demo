6df75b9870917f42352612a934fe152bf1348a2a
Laura Verney <laura.verney@bell.ca>
2025-04-24 16:36:38 -0300

    Update Jobs component to implement useGetJobsQuery

--- ui-service/src/routes/jobs/Jobs.tsx
+++ ui-service/src/routes/jobs/Jobs.tsx
@@ -1,8 +1,10 @@
 import { Box, Typography, Grid } from "@mui/material";
 import JobsDataGrid from "src/routes/jobs/JobsDataGrid";
+import { useGetJobsQuery } from "src/modules/jobService/jobsApiSlice";
 import { getJobsResponse } from "src/modules/jobService/mocks";
 
 const Jobs = () => {
+  const { data, isError } = useGetJobsQuery();
   return (
     <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
       <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
@@ -10,7 +12,7 @@ const Jobs = () => {
       </Typography>
       <Grid container spacing={2} columns={12}>
         <Grid size={{ xs: 12, lg: 9 }}>
-          <JobsDataGrid jobs={getJobsResponse} />
+          <JobsDataGrid jobs={isError ? getJobsResponse : data ?? []} />
         </Grid>
       </Grid>
     </Box>
