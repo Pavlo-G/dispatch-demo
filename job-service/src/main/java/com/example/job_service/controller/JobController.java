eeee5df75a127f49f1df51f21734be38f063b1e3
Pavlo-G <6306260@gmail.com>
2025-04-24 15:05:05 -0400

    add  @CrossOrigin

--- job-service/src/main/java/com/example/job_service/controller/JobController.java
+++ job-service/src/main/java/com/example/job_service/controller/JobController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/jobs")
 public class JobController {
     Logger logger = LoggerFactory.getLogger(JobController.class);
